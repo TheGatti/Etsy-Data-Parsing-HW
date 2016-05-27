2d3ab0ce39c98b056489447687988bde4886ddbc
David Ryan Delli-Gatti <Gatti@Davids-MacBook-Pro.local>
2016-05-27 12:27:45 -0400

    fixed mambo#5

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function sum(arr) {
    return sumOfNums;
  }
 sum(prices) / prices.length
+document.getElementById("answer1").innerHTML= sum(prices) / prices.length;
 
-//document.getElementById("answer1").innerHTML=avg;
 //answer 2
 var middleFilter = items.filter (function (element,idx,arr) {
   return element.price >= 14.00 && element.price <= 18.00 && element.currency_code === "USD"
@@ -22,28 +22,34 @@ var middleFilter = items.filter (function (element,idx,arr) {
 });
 console.log (middleFilter);
 document.getElementById("answer2").innerHTML=middleFilter;
+
 //answer 3
-var gbpFilter = items.filter (function (element,idx,arrr) {
+var gbpFilter = items.filter (function (element,idx,arr) {
   return element.currency_code === "GBP"
 }).map (function (element,idx,arr) {
-  return element.title && element.price
+  return element.title + ", " + element.price;
 });
 console.log (gbpFilter);
-//document.getElementById("answer3").innerHTML=gpbFilter;
+document.getElementById("answer3").innerHTML=gbpFilter;
+
 //answer 4
 var woodFilter = items.filter(function(element,idx,arr) {
   return element.materials.indexOf("wood") !== -1
 }).map (function(element,idx,arr){
   return element.title;
 });
-console.log (woodFilter)
+console.log (woodFilter);
+document.getElementById("answer4").innerHTML=woodFilter;
+
 //answer 5
 var eightPlus = items.filter(function(element,idx,arr){
-  return element.materials <= 8
+  return element.materials.length >= 8;
 }).map(function(element,idx,arr) {
-  return element.title + ", " + element.quantity + ", " + element.materials
+  return element.title + ", " + element.quantity + ", " + element.materials;
 });
 console.log (eightPlus);
+document.getElementById("answer5").innerHTML=eightPlus;
+
 //answer 6
 var manyItems = items.filter(function(element,idx,arr){
   return element.who_made === "i_did"
